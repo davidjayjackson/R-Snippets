71fd68e437e338b99437edb010c3a073edc7ad36
davidjayjackson <46544155+davidjayjackson@users.noreply.github.com>
2020-02-24 21:21:08 -0500

    Update plot_ly.R

--- plot_ly.R
+++ plot_ly.R
@@ -7,3 +7,11 @@ plot_ly(fcc) %>% add_lines(x=~Ymd,y=~Yhat) %>%
   add_lines(data=A,x=~Ymd,y=~MA) %>%
   layout(yaxis=list(autorange="reversed"))
 
+## kent.johnson @ Slack rading file names into R.
+Break up the code so you capture the result of choose.files. 
+Then use basename to get the name of the file. E.g.
+
+path = choose.files()
+name = basename(path)
+betelguese = fread(path)
+
